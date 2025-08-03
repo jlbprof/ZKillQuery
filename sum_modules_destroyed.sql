7502f462381893cae698250289abd28c423c8ccd
Julian Brown <julian@jlbprof.com>
2025-08-02 21:29:48 -0500

    SQL to get modules destroyed and quantities.

---
+++ sum_modules_destroyed.sql
@@ -0,0 +1,15 @@
+SELECT 
+    itt.typeID,
+    itt.typeName,
+    SUM(di.quantity) as total_quantity
+FROM 
+    droppedItems di
+JOIN
+    invTypes itt ON itt.typeID = di.typeID
+WHERE 
+    1
+GROUP BY
+    itt.typeID,
+    itt.typeName
+ORDER BY 
+    itt.typeName
