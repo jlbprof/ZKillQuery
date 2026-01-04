c20afc05473749fec984c94803bc4d5d03cfae96
Julian Brown <julian@jlbprof.com>
2026-01-04 17:11:27 -0600

    wip

--- podman_script.sh
+++ podman_script.sh
@@ -3,7 +3,7 @@
 set -x
 
 podman-compose down || true
-podman images | grep zkill | awk '{print $1}' | xargs podman rmi || true
+podman images | grep zkill | awk '{print $3}' | xargs podman rmi || true
 
 podman-compose build
 podman-compose up -d
